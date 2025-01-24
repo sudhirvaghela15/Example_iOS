9dec0317b82b7c335f5ff1c2966eb486cc4819fe
sudhir <sudhirvaghela15@hotmail.com>
2025-01-24 18:48:56 +0530

    creating home folder model

--- SOLID_principles_Example_iOS/Model/CardModel.swift
+++ SOLID_principles_Example_iOS/Model/CardModel.swift
@@ -6,29 +6,33 @@
 //
 
 
+struct HomeFolder {
+	var type: String
+	var items: [CardModel]
+}
+
 struct CardModel {
     var title: String!
     var description: String!
     var active: Bool!
 }
 
-var dummyCardModelArrayOne = [
+var dummyHomeFolderOne = HomeFolder(type: "1 Section", items: [
 	CardModel(title: "Title 1", description: "Description 1", active: false),
 	CardModel(title: "Title 2", description: "Description 2", active: true),
 	CardModel(title: "Title 3", description: "Description 5", active: false),
 	CardModel(title: "Title 4", description: "Description 6", active: false)
+])
 
-]
-
-var dummyCardModelArrayTwo = [
+var dummyHomeFolderTwo = HomeFolder(type: "2 Section", items: [
 	CardModel(title: "Title 5", description: "Description 5", active: false),
 	CardModel(title: "Title 6", description: "Description 6", active: false),
-	CardModel(title: "Title 7", description: "Description 8", active: false)
-]
+	CardModel(title: "Title 7", description: "Description 7", active: false)
+])
 
-var dummyCardCardModelArrayThree = [
-	CardModel(title: "Title 10", description: "Description 10", active: false),
-	CardModel(title: "Title 11", description: "Description 11", active: true),
-	CardModel(title: "Title 12", description: "Description 12", active: true),
-	CardModel(title: "Title 13", description: "Description 13", active: true)
-]
+var dummyHomeFolderThree = HomeFolder(type: "3 Section", items: [
+	CardModel(title: "Title 8", description: "Description 8", active: false),
+	CardModel(title: "Title 9", description: "Description 9", active: true),
+	CardModel(title: "Title 10", description: "Description 10", active: true),
+	CardModel(title: "Title 11", description: "Description 11", active: true)
+])
