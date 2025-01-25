aad27b30c16732993dccf7a93114204c655048ee
sudhir <sudhirvaghela15@hotmail.com>
2025-01-25 12:32:40 +0530

    card two cell controlller added

---
+++ SOLID_principles_Example_iOS/Modules/Feed/Controllers/Card2CellController.swift
@@ -0,0 +1,32 @@
+//
+//  CardCellController 2.swift
+//  SOLID_principles_Example_iOS
+//
+//  Created by sudhir on 25/01/25.
+//
+
+import UIKit
+
+final class Card2CellController: CellController {
+	
+	var model: CardModel
+	
+	init(model: CardModel) {
+		self.model = model
+	}
+	
+	func register(_ tableView: UITableView) {
+		tableView.register(
+			UINib(nibName: "Card2Cell", bundle: nil),
+			   forCellReuseIdentifier: "Card2Cell"
+		   )
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Card2Cell", for: indexPath) as! Card2Cell
+	
+		cell.configCell(item: model)
+		cell.backgroundColor = .green
+		return cell
+	}
+}
